2f4c9dac44180a2a3d9004f86c1eb3a406663856
jaykhandelwal22 <jaykhandelwal22@gmail.com>
2021-04-11 01:28:21 +0530

    about us ui

---
+++ app/src/main/java/com/example/ratatouille/Activity/AboutUsActivity.java
@@ -0,0 +1,16 @@
+package com.example.ratatouille.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ratatouille.R;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
