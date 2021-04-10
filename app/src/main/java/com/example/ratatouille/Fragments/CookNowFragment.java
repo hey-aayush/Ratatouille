969de04a952bc6c08c2cfc097732caf43f2ae3f4
jaykhandelwal22 <jaykhandelwal22@gmail.com>
2021-04-10 14:06:57 +0530

    fragment added

---
+++ app/src/main/java/com/example/ratatouille/Fragments/CookNowFragment.java
@@ -0,0 +1,8 @@
+package com.example.ratatouille.Fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class CookNowFragment extends Fragment {
+    public CookNowFragment() {
+    }
+}
---
+++ app/src/main/java/com/example/ratatouille/Fragments/WhatTodayFragment.java
@@ -0,0 +1,10 @@
+package com.example.ratatouille.Fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class WhatTodayFragment extends Fragment {
+
+
+    public WhatTodayFragment() {
+    }
+}
